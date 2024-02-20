1a943fc4dd327ad5273f5b8506342309d0959aca
shubhansh-simple <shubhansh7777@gmail.com>
2024-02-20 18:08:16 +0530

    Improve current code visibility

--- request_counter/tests.py
+++ request_counter/tests.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from rest_framework      import status
 from rest_framework.test import APITestCase
 
+
 class RequestCountApiViewTest(APITestCase):
 
     # Get the URL for the RequestCountApiView
@@ -15,11 +16,16 @@ class RequestCountApiViewTest(APITestCase):
     def test_authentication_failure_without_token(self):
         """Failure Case: Server Authentication Endpoints Not Implemented Successfully"""
 
-        # Make a GET request to the endpoint
         response = self.client.get(self.base_url)
 
+        actual_status_code   = response.status_code
+        expected_status_code = status.HTTP_401_UNAUTHORIZED
+
         # Check that status code is 401 Unauthorized
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED, f'Expected status code 401, but got {response.status_code}')
+        self.assertEqual(
+                actual_status_code,
+                expected_status_code, 
+                f'Expected status code {expected_status_code}, but got {actual_status_code}')
 
 
     def test_authentication_success_with_token(self):
@@ -33,16 +39,27 @@ class RequestCountApiViewTest(APITestCase):
                 'password' : '123456'
         }
 
+        # GET TOKEN
         jwt_response = self.client.post(token_url, data=payload_data, format='json')
 
-        # Check that status code is 200 OK
-        self.assertEqual(jwt_response.status_code, status.HTTP_200_OK, f'Expected status code 200, but got {jwt_response.status_code}')
+        actual_status_code   = jwt_response.status_code
+        expected_status_code = status.HTTP_200_OK
+
+        self.assertEqual(
+                actual_status_code,
+                expected_status_code, 
+                f'Expected status code {expected_status_code}, but got {actual_status_code}')
+
 
         auth_header = f'Bearer {jwt_response.data["access_token"]}'
+
         response    = self.client.get(self.base_url, HTTP_AUTHORIZATION=auth_header)
 
         # Check that status code is 200 OK
-        self.assertEqual(response.status_code, status.HTTP_200_OK, f'Expected status code 200, but got {response.status_code}')
+        self.assertEqual(
+                response.status_code, 
+                status.HTTP_200_OK, 
+                f'Expected status code 200, but got {response.status_code}')
 
         # Assert that the key 'requests' exists in the response data dictionary
         self.assertIn('requests',response.data)
