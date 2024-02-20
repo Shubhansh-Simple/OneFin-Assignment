4f482342bac2d926d50b31b0cada72ea58caac15
shubhansh-simple <shubhansh7777@gmail.com>
2024-02-20 05:47:46 +0530

    Tested With PostMan

--- config/settings.py
+++ config/settings.py
@@ -140,9 +140,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    #'DEFAULT_PERMISSION_CLASSES' : [
-    #    'rest_framework.permissions.AllowAny',
-    #]
 }
 
 # Custom Life Span Of Token
--- users/api/urls.py
+++ users/api/urls.py
@@ -3,15 +3,10 @@
 # django 
 from django.urls import path
 
-# rest_framework
-from rest_framework_simplejwt import views as jwt_view
-
 # local
 from .views import RegisterApi
 
-
 urlpatterns = [
     path('register/', RegisterApi.as_view() ),
-    path('login/', jwt_view.TokenObtainPairView.as_view(), name='token_obtain' ),
 ]
 
